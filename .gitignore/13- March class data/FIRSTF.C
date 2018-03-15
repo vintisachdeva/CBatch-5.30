538bf9da4aba683aaca0100ae9887f3965f2d581
vintisachdeva <vintisachdeva17@gmail.com>
2018-03-15 18:01:20 +0530

    Add files via upload

---
+++ .gitignore/13- March class data/FIRSTF.C
@@ -0,0 +1,10 @@
+void main(){
+	int value2 = 10, value = 9, sum; //garbage value
+	clrscr();
+	sum = value + value2;
+	printf("value = %d%d%d");
+	printf("value2 = %d");
+	printf("sum = %d");
+	getch();
+
+}
